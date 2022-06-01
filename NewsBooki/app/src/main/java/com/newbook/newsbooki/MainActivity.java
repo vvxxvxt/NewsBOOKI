199e7b645f02653bfe8b0d7570a322b9e297c34c
seunghoon <hoon8356@hansung.ac.kr>
2022-06-02 00:58:30 +0900

    사이드 메뉴바 추가

--- NewsBooki/app/src/main/java/com/newbook/newsbooki/MainActivity.java
+++ NewsBooki/app/src/main/java/com/newbook/newsbooki/MainActivity.java
@@ -3,6 +3,7 @@ package com.newbook.newsbooki;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -10,7 +11,14 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -18,38 +26,28 @@ public class MainActivity extends AppCompatActivity {
 
     private int listnumber = 10;
     private ListData[] listData = new ListData[listnumber];
-    public MainActivity(){
-        ListInput();
-    }
+
     private void ListInput() {
-        for(int i=0;i<10;i++) {
+        for (int i = 0; i < 10; i++) {
             listData[i] = new ListData("2022학년도 1학기 폐강공고 및 폐강과목 수강신청자 수강정정 안내" + i, "작성일:2022.05.04");
         }
     }
 
-    private MenuData[] menuData = {
-            new MenuData(R.drawable.rect43, R.drawable.notice_pressed, R.drawable.noticeicon_pressed),
-            new MenuData(R.drawable.rect44, R.drawable.news, R.drawable.newsicon),
-            new MenuData(R.drawable.rect44, R.drawable.schedule, R.drawable.scheduleicon),
-            new MenuData(R.drawable.rect44, R.drawable.my, R.drawable.myicon)
-    };
-
     private ListView mList;
     private com.newbook.newsbooki.MyAdapter mAdapter;
-    private GridView menuView;
-    private MyMenuAdapter menuAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ListInput();
         setContentView(R.layout.activity_main);
 
         mList = (ListView) findViewById(R.id.list);
         mAdapter = new com.newbook.newsbooki.MyAdapter(this, listData);
         mList.setAdapter(mAdapter);
 
-        //클릭시 리스너
 
+        //리스트 클릭시, 글 내용이 나오게 하는 리스너
         mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -58,11 +56,77 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        this.InitializeLayout();
+    }
+
+    public void InitializeLayout() {
+        //toolBar를 통해 App Bar 생성
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("학사공지");
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayShowTitleEnabled(false);
+        //앱이름이 툴바에 나오는데, 그걸 제거함.
+        //toolbar.setTitle같은거로 제목 설정이 될거같긴한데 걍 텍스트뷰 사용하는게 나을거같음.
+
+        //App Bar의 좌측 영영에 Drawer를 Open 하기 위한 Incon 추가
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu_button);
+
+        DrawerLayout drawLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 
 
-        menuView = (GridView) findViewById(R.id.menu);
-        menuAdapter = new MyMenuAdapter(this, menuData);
-        menuView.setAdapter(menuAdapter);
+
+
+
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawLayout,
+                toolbar,
+                R.string.open,
+                R.string.closed
+        );
+
+        drawLayout.addDrawerListener(actionBarDrawerToggle);
+
+
+
+
+
+
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.menuitem1:
+                        Toast.makeText(getApplicationContext(), "학사공지", Toast.LENGTH_SHORT).show();
+                    case R.id.menuitem2:
+                        Toast.makeText(getApplicationContext(), "My", Toast.LENGTH_SHORT).show();
+                    case R.id.menuitem3:
+                        Toast.makeText(getApplicationContext(), "뉴스", Toast.LENGTH_SHORT).show();
+                    case R.id.menuitem4:
+                        Toast.makeText(getApplicationContext(), "시간표", Toast.LENGTH_SHORT).show();
+                }
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
 
     }
 }
